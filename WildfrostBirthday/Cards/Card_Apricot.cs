453104f7d756503ee6561c0ce3d85f6aaec9922d
Caleb <Malmaster449@outlook.com>
2025-05-23 23:57:40 -0700

    Refactor code structure for improved readability and maintainability

---
+++ WildfrostBirthday/Cards/Card_Apricot.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WildfrostBirthday.Cards
+{
+    public static class Card_Apricot
+    {
+        public static void Register(WildFamilyMod mod)
+        {
+            // First, register the phase transition status effect
+            var phaseBuilder = new StatusEffectDataBuilder(mod)
+                .Create<StatusEffectNextPhase>("ShellBossPhase2")
+                .WithStackable(false)
+                .WithCanBeBoosted(false)
+                .WithOffensive(false)
+                .WithMakesOffensive(false)
+                .WithDoesDamage(false)
+                .WithType("nextphase")
+                .SubscribeToAfterAllBuildEvent<StatusEffectNextPhase>(data =>
+                {
+                    data.preventDeath = true;
+                    data.nextPhase = mod.TryGet<CardData>("apricot_2");
+                    // Let the game handle the default transformation animation
+                    data.animation = null;
+                });
+
+            mod.assets.Add(phaseBuilder);
+
+            string cardId = "apricot";
+            string spritePath = "enemies/apricot";
+
+            var builder = new CardDataBuilder(mod)
+                .CreateUnit(cardId, "Apricot")
+                .SetSprites(spritePath + "apricot.png", "bg.png")
+                .SetStats(5, 5, 6)  // HP: 5, ATK: 5, Counter: 6
+                .WithFlavour("A master of shell manipulation, turning defense into devastating offense.")
+                .WithCardType("Boss")
+                .SubscribeToAfterAllBuildEvent(data =>
+                {
+                    // Starting effects
+                    data.startWithEffects = new[] {
+                        mod.SStack("ImmuneToSnow", 1),
+                        mod.SStack("Shell", 10),
+                        mod.SStack("ShellBossPhase2", 1),
+                        mod.SStack("Bonus Damage Equal To Shell", 1)
+                    };
+
+                    // Set traits - Backline
+                    data.traits = new List<CardData.TraitStacks> {
+                        mod.TStack("Backline", 1)
+                    };
+                });
+
+            mod.assets.Add(builder);
+
+            // Create Apricot Phase 2 variant
+            string cardId2 = "apricot_2";
+            
+            var builder2 = new CardDataBuilder(mod)
+                .CreateUnit(cardId2, "Apricot")
+                .SetSprites(spritePath + "apricot_2.png", "bg.png")
+                .SetStats(5, 1, 1)  // HP: 5, ATK: 1, Counter: 1
+                .WithFlavour("Now focusing purely on defense, Apricot unleashes devastating area attacks.")
+                .WithCardType("Boss")
+                .SubscribeToAfterAllBuildEvent(data =>
+                {
+                    // Starting effects
+                    data.startWithEffects = new[] {
+                        mod.SStack("ImmuneToSnow", 1),
+                        mod.SStack("Shell", 25),
+                        mod.SStack("Hit All Enemies", 1)
+                    };
+
+                    // Set traits - Frontline
+                    data.traits = new List<CardData.TraitStacks> {
+                        mod.TStack("Frontline", 1)
+                    };
+                });
+
+            mod.assets.Add(builder2);
+        }
+    }
+}
--- WildfrostBirthday/Cards/Card_FrostKnight.cs
+++ WildfrostBirthday/Cards/Card_FrostKnight.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using WildfrostBirthday.Helpers;
@@ -5,11 +6,44 @@ using WildfrostBirthday.Helpers;
 
 namespace WildfrostBirthday.Cards
 {
-    public static class Card_FrostKnight
+    /// <summary>
+    /// Helper class for creating scriptable objects with initialization
+    /// </summary>
+    public class Scriptable<T> where T : ScriptableObject, new()
     {
+        readonly Action<T> modifier;
+        public Scriptable() { }
+        public Scriptable(Action<T> modifier) { this.modifier = modifier; }
+        public static implicit operator T(Scriptable<T> scriptable)
+        {
+            T result = ScriptableObject.CreateInstance<T>();
+            scriptable.modifier?.Invoke(result);
+            return result;
+        }
+    }
+
+    public static class Card_FrostKnight
+    {        
         public static void Register(WildFamilyMod mod)
         {
-            string cardId = "frost_knight";
+            // First, register the phase transition status effect
+            var phaseBuilder = new StatusEffectDataBuilder(mod)
+                .Create<StatusEffectNextPhase>("FrostBossPhase2")
+                .WithStackable(false)
+                .WithCanBeBoosted(false)
+                .WithOffensive(false)
+                .WithMakesOffensive(false)
+                .WithDoesDamage(false)
+                .WithType("nextphase")
+                .SubscribeToAfterAllBuildEvent<StatusEffectNextPhase>(data =>
+                {
+                    data.preventDeath = true;
+                    data.nextPhase = mod.TryGet<CardData>("frost_knight_2");
+                    // Let the game handle the default transformation animation
+                    data.animation = null;
+                });
+
+            mod.assets.Add(phaseBuilder);            string cardId = "frost_knight";
             string spritePath = "enemies/frost_knight";
 
             var builder = new CardDataBuilder(mod)
@@ -22,7 +56,8 @@ namespace WildfrostBirthday.Cards
                 {
                     // Start with ImmuneToSnow 1
                     data.startWithEffects = new[] {
-                        mod.SStack("ImmuneToSnow", 1)
+                        mod.SStack("ImmuneToSnow", 1),
+                        mod.SStack("FrostBossPhase2", 1)
                     };
 
                     // Attack effect: Apply 10 Frost
@@ -32,6 +67,29 @@ namespace WildfrostBirthday.Cards
                 });
 
             mod.assets.Add(builder);
+
+            // Create Frost Knight 2 variant
+            string cardId2 = "frost_knight_2";            var builder2 = new CardDataBuilder(mod)
+                .CreateUnit(cardId2, "The Frost Knight")
+                .SetSprites(spritePath + "frost_knight_2.png", "bg.png")
+                .SetStats(60, 2, 3)  // HP: 60, ATK: 2, Counter: 3
+                .WithFlavour("A more agile variant of the Frost Knight, darting around with rapid strikes.")
+                .WithCardType("Boss")
+                .SubscribeToAfterAllBuildEvent(data =>
+                {
+                    // Starting effects: ImmuneToSnow 1, MultiHit 3
+                    data.startWithEffects = new[] {
+                        mod.SStack("ImmuneToSnow", 1),
+                        mod.SStack("MultiHit", 3)
+                    };
+
+                    // Set traits - Aimless
+                    data.traits = new List<CardData.TraitStacks> {
+                        mod.TStack("Aimless", 1)
+                    };
+                });
+
+            mod.assets.Add(builder2);
         }
     }
 }
