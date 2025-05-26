4dc07ad979aab6cc20e3f8177f064eaf710ae48e
Caleb <Malmaster449@outlook.com>
2025-05-25 19:17:00 -0700

    This is for my father to help me!

---
+++ WildfrostBirthday/Effects/StatusEffect_InstantSummonLuminFragmentInHand.cs
@@ -0,0 +1,28 @@
+// Registers the "Instant Summon LuminFragment" effect for the mod.
+// No usings needed; all required namespaces are provided by GlobalUsings.cs
+ 
+//namespace WildfrostBirthday.Effects
+// {
+//     public static class StatusEffect_InstantSummonLuminFragment
+//     {
+//         /// <summary>
+//         /// Registers the "Instant Summon LuminFragment" effect.
+//         /// </summary>
+//         public static void Register(WildfrostBirthday.WildFamilyMod mod)
+//         {
+//              var builder = new StatusEffectDataBuilder(mod)
+//                 .Create<StatusEffectInstantSummon>("Instant Summon LuminFragment")
+//                 .WithStackable(false)
+//                  .WithCanBeBoosted(false)
+//                  .WithOffensive(false)
+//                  .WithMakesOffensive(false)
+//                  .WithDoesDamage(false)
+//                  .SubscribeToAfterAllBuildEvent<StatusEffectInstantSummon>(data =>
+//                 {
+//                      data.eventPriority = 99999;
+//                      data.targetSummon = mod.TryGet<StatusEffectSummon>("Summon LuminFragment");
+//                 });
+//              mod.assets.Add(builder);
+//         }
+//     }
+//  }
